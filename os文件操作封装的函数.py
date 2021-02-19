3cf723735a3358f83055ede472eceef4fa8cd241
董董 <772483200@qq.com>
2021-02-19 11:28:20 +0800

    Update os文件操作封装的函数.py

--- os文件操作封装的函数.py
+++ os文件操作封装的函数.py
@@ -1,6 +1,28 @@
 # ‐*‐coding:utf‐8‐*‐
 import os
 
+
+os.path.abspath(path)：获取目标位置的绝对路径
+os.path.exists(path)：判断目标是否存在
+os.path.isdir(path)：判断目标是否是目录
+os.path.isfile(path)：判断目标是目录还是文件
+os.walk(path)：遍历指定目录下的所有子文件夹和子文件夹中的所有文件
+os.listdir()：返回指定路径下所有的文件和文件夹列表,但是子目录下文件不遍历
+os.path.basename(file)： 返回文件名(不带路径带后缀)
+
+os.path.join()：将分离的部分合成一个整体
+filename=os.path.join('/home/ubuntu/python_code','split_func') ：输出为：/home/ubuntu/python_code/split_func
+
+os.path.splitext()：将文件名和扩展名分开，不会去除路径
+fname,fename=os.path.splitext('/home/ubuntu/python_code/split_func/split_function.py')#输出为：/home/ubuntu/python_code/split_func/split_function   .py
+ 
+#os.path.split（）返回文件的路径和文件名
+dirname,filename=os.path.split('/home/ubuntu/python_code/split_func/split_function.py')# /home/ubuntu/python_code/split_func     split_function.py
+
+# shutil库
+shutil.move(src, dst)：把文件移动到指定位置
+
+
 # os.listdir递归获取所有的目录和文件(含路径)
 def get_file_path(root_path, file_list, dir_list):
     # 获取该目录下所有的文件名称和目录名称
