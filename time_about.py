ee7b7078117126885a13f7483598cc551ec1e468
董董 <772483200@qq.com>
2021-10-11 16:17:00 +0800

    Update time_about.py

--- time_about.py
+++ time_about.py
@@ -28,3 +28,33 @@ def get_time_unix(num):
         the_day_unix = int(round(the_day_unix)) * 1000
         unix_times.append(the_day_unix)
     return unix_times
+
+
+# 格式化今天的日期
+from datetime import date
+
+today = date.today()
+# dd/mm/YY
+d1 = today.strftime("%d/%m/%Y")
+print("d1 =", d1)
+
+# Textual month, day and year	
+d2 = today.strftime("%B %d, %Y")
+print("d2 =", d2)
+
+# mm/dd/y
+d3 = today.strftime("%m/%d/%y")
+print("d3 =", d3)
+
+# Month abbreviation, day and year	
+d4 = today.strftime("%b-%d-%Y")
+print("d4 =", d4)
+
+# 正则匹配日期
+import datetime
+from datetime import date
+import re
+s = "Jason's birthday is on 1991-09-21 1991-9-21"
+match = re.search(r'\d{4}-\d{1,2}-\d{1,2}', s)
+date = datetime.datetime.strptime(match.group(), '%Y-%m-%d').date()
+print(date)
