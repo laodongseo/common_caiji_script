b2fe15138e31b7437aea867950f17cf4543ce98f
董董 <772483200@qq.com>
2021-11-04 16:24:06 +0800

    Update do_img.py

--- do_img.py
+++ do_img.py
@@ -39,3 +39,30 @@ dsize = (width, height)
 # resize image
 output = cv2.resize(src, dsize)
 cv2.imwrite('D:/火车浏览器/cv2-resize-image-50.png',output) 
+
+
+import cv2
+
+src = cv2.imread('D:/cv2-resize-image-original.png', cv2.IMREAD_UNCHANGED)
+
+# set a new width in pixels
+new_width = 300
+# dsize
+dsize = (new_width, src.shape[0])
+# resize image
+output = cv2.resize(src, dsize, interpolation = cv2.INTER_AREA)
+cv2.imwrite('D:/cv2-resize-image-width.png',output) 
+
+
+import cv2
+ 
+src = cv2.imread('D:/cv2-resize-image-original.png', cv2.IMREAD_UNCHANGED)
+
+# set a new height in pixels
+new_height = 200
+
+# dsize
+dsize = (src.shape[1], new_height)
+# resize image
+output = cv2.resize(src, dsize, interpolation = cv2.INTER_AREA)
+cv2.imwrite('D:/cv2-resize-image-height.png',output) 
