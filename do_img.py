00f0f2b6f3a85770d842070eb0be3fdffc16d147
董董 <772483200@qq.com>
2021-11-04 16:22:21 +0800

    Update do_img.py

--- do_img.py
+++ do_img.py
@@ -19,3 +19,23 @@ wsize = int((float(img.size[0]) * float(hpercent)))
 img = img.resize((wsize, baseheight), Image.ANTIALIAS)
 img.save('resized_image.jpg')
 ------------------------------------------------------------------------------
+"""
+pip install opencv-python
+"""
+import cv2
+
+path = r"./ytest/540.png" # 不可以是中文路径
+src = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+
+#percent by which the image is resized
+scale_percent = 50
+
+#calculate the 50 percent of original dimensions
+width = int(src.shape[1] * scale_percent / 100)
+height = int(src.shape[0] * scale_percent / 100)
+
+# dsize
+dsize = (width, height)
+# resize image
+output = cv2.resize(src, dsize)
+cv2.imwrite('D:/火车浏览器/cv2-resize-image-50.png',output) 
