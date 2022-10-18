324e854db88c9440b8764eb32b323d8feb7a3d21
董董 <772483200@qq.com>
2022-10-18 18:08:57 +0800

    Update from_link_caiji_content.py

--- from_link_caiji_content.py
+++ from_link_caiji_content.py
@@ -1,6 +1,6 @@
 # ‐*‐ coding: utf‐8 ‐*‐
 """
-从url的txt里采集每篇内容
+从excel的url列里采集每篇内容
 """
 
 import requests
@@ -9,87 +9,91 @@ import threading
 import queue
 import time
 import gc
-import re
-import random
+import re,chardet
+import random,traceback
 from urllib.parse import urlparse
+import pandas as pd
 
 
 class Spider_Content(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
+	def __init__(self):
+		threading.Thread.__init__(self)
 
-    @staticmethod
-    def read(filepath):
-        q = queue.Queue()
-        for i in open(filepath, 'r', encoding='utf-8'):
-            i = i.strip().split('\t')[0]
-            q.put(i)
-        return q
+	@staticmethod
+	def read(filepath):
+		q = queue.Queue()
+		for index,row in pd.read_excel(filepath).iterrows():
+			q.put(row)
+		return q
 
-    # 获取源码
-    def get_html(self, url, retry=2):
-        try:
-            r = requests.get(url=url, headers=user_agent, timeout=5)
-        except Exception as e:
-            print('获取源码失败', url, e)
-            time.sleep(30)
-            if retry > 0:
-                self.get_html(url, retry - 1)
-        else:
-            html = r.text
-            return html
+	# 获取源码
+	def get_html(self, url, retry=1):
+		try:
+			r = requests.get(url=url, headers=user_agent, timeout=15)
+		except Exception as e:
+			print('获取源码失败', url, e)
+			time.sleep(30)
+			if retry > 0:
+				self.get_html(url, retry - 1)
+		else:
+			data = r.content
+			code = chardet.detect(data)['encoding']
+			html = data.decode(code,errors='ignore')
+			return html
 
-    def parse(self, html):
-        if html:
-            doc= pq(str(html))
-            title = doc('h1').text().replace('?','').replace('？','').replace('/','')
-            p_list = doc('.detailc p').items()
-            f = open('{0}{1}.txt'.format(save_path,title), 'w',encoding='utf-8')
-            for p in p_list:
-                duanluo = p.text().strip()
-                # duan = str(p)  # 一行
-                # if '<a' in duan:
-                #     duan = re.sub('<a href=".*?16px;">','<a>',duan)
-                # if 'img' in duan:
-                #     num = random.randint(1,200)
-                #     duan = re.sub('src=".*?.jpg"','src="/jiandian1/{0}xiu.jpg"'.format(num),duan)
-                # duan = duan.replace('http://www.wanshifu.com','').replace('万师傅','天地一修')
-                # 图片不写入
-                if duanluo:
-                    f.write(duanluo + '\n')
-            f.flush()
-            f.close()
-            time.sleep(10)
-            print('ok一个')
+	def parse(self, html):
+		doc= pq(str(html))
+		p_list = doc('.info-zi p').items()
+		texts = []
+		for p in p_list:
+			text = p.text().strip()
+			texts.append(text) if text else True
+		article = ''.join([f'<p>　　{text}</p>' for text in texts])
+		return article
 
-    # 统计每个域名排名的词数
-    def run(self):
-        while 1:
-            url = q.get()
-            print(url)
-            try:
-                html = self.get_html(url)
-                self.parse(html)
-            except Exception as e:
-                print(e)
-            finally:
-                q.task_done()
-                gc.collect()
+
+	def run(self):
+		global IsHeader
+		while 1:
+			row = q.get()
+			url = row['url']
+			print(url)
+			try:
+				html = self.get_html(url)
+				article = self.parse(html)
+			except Exception as e:
+				traceback.print_exc()
+			else:
+				if isinstance(article,str) and len(article) > 0:
+					row['article'] = article
+					df = row.to_frame().T
+					with lock:
+						if IsHeader == 0:
+							df.to_csv(TodayCsvFile,encoding='utf-8-sig',mode='w+',index=False)
+							IsHeader = 1
+						else:
+							df.to_csv(TodayCsvFile,encoding='utf-8-sig',mode='a+',index=False,header=False)
+			finally:
+				q.task_done()
+				gc.collect()
+				time.sleep(2)
 
 
 if __name__ == "__main__":
-    start = time.time()
-    save_path = './39/'
-    user_agent = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
-    q = Spider_Content.read('link_title_baidianfeng.txt')
+	start = time.time()
+	user_agent = {'User-Agent': 'Sogou web spider/4.0(+http://www.sogou.com/docs/help/webmasters.htm#07)'}
+	q = Spider_Content.read('txt合并.xlsx')
+	TodayCsvFile = f'res_new.csv'
+	lock = threading.Lock()
+	IsHeader = 0
 
-    # 设置线程数
-    for i in list(range(1)):
-        t = Spider_Content()
-        t.setDaemon(True)
-        t.start()
-    q.join()
+	# 设置线程数
+	for i in list(range(1)):
+		t = Spider_Content()
+		t.setDaemon(True)
+		t.start()
+	q.join()
 
-    end = time.time()
+	end = time.time()
+	print((end - start))
 
