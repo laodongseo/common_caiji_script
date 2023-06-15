85d05b354aac04eaac9616c10bc591e3698296f6
董董 <772483200@qq.com>
2023-06-15 14:31:59 +0800

    Update liebiao_multi_start_caiji.py

--- liebiao_multi_start_caiji.py
+++ liebiao_multi_start_caiji.py
@@ -1,3 +1,5 @@
+# ‐*‐ coding: utf‐8 ‐*‐
+
 """
 配置多个列表,指定起始页采集
 抓取下一页的链接放到url队列
@@ -10,15 +12,18 @@ import threading
 import queue
 from pyquery import PyQuery as pq
 import random
-import time
+import pandas as pd
+import time,os
 import traceback
+import gzip
+from io import BytesIO
 
 
 def read_excel(filepath):
     q = queue.Queue()
     df = pd.read_excel(filepath).dropna()
     for index,row in df.iterrows():
-        q.put(row)
+        q.put(row['url'])
     return q
 
 
@@ -32,64 +37,70 @@ def get_html(url,retry=3):
             time.sleep(30)
             get_html(url, retry - 1)
     else:
-        html = r.text
         url = r.url
+        if r.headers.get('Content-Encoding') == 'gzip':
+            html = gzip.GzipFile(fileobj=BytesIO(r.content)).read().decode('utf-8',errors='ignore')
+        else:
+            html = r.content.decode('utf-8',errors='ignore')
         return html,url
 
 
 def parse_html(html,url):
     doc = pq(str(html))
-    page_next = doc('.pagenxt')
-    if page_next:
-        next_page = page_next.attr('href')
-        print(next_page)
-        q.put(next_page)
+    li_obj = doc('.pagination li').eq(-1)
+    if li_obj:
+        next_page = li_obj('a').attr('href')
+        print('\t',next_page)
+        q.put('https://www.yutu.cn' + next_page)
     else:
         print('无下一页')
     # 采集内容
     content = []
-    list_a = doc('.content_box .right ul li a').items()
+    list_a = doc('.comm-soft-lists div.comm-item h2 a.h2-tite').items()
     for a in list_a:
-        text = a('.question').text()
+        text = a.text().strip()
         link = a.attr('href')
-        print(text,link)
+        print(text)
         content.append((text,link))
-    f.flush()
+    return content
 
 
 def main():
     global IsHeader
     while 1:
-        row = q.get()
-        url = row['url']
+        url = q.get()
+        print(url)
         try:
             html,now_url = get_html(url)
             content = parse_html(html,now_url)
         except Exception as e:
             traceback.print_exc() 
         else:
-            if isinstance(content,tuple):
-                row['title'] ,row['content'] = content
-                df = row.to_frame().T
-                with lock:
-                    if IsHeader == 0:
-                        df.to_csv(CsvFile,encoding='utf-8-sig',mode='w+',index=False,sep=',')
-                        IsHeader = 1
-                    else:
-                        df.to_csv(CsvFile,encoding='utf-8-sig',mode='a+',index=False,sep=',',header=False)
+            if isinstance(content,list):
+                for element in content:
+                    row = pd.Series(dtype=object)
+                    row['title'] ,row['content'] = element
+                    row['list_url'] = url
+                    df = row.to_frame().T
+                    with lock:
+                        if IsHeader == 0:
+                            df.to_csv(CsvFile,encoding='utf-8-sig',mode='w+',index=False,sep=',')
+                            IsHeader = 1
+                        else:
+                            df.to_csv(CsvFile,encoding='utf-8-sig',mode='a+',index=False,sep=',',header=False)
         finally:
             q.task_done()
-            time.sleep(5)
+            time.sleep(3)
 
 
 if __name__ == "__main__":
     my_header = {
-'Host': 'www.loupan.com',
-'Referer': 'http://bj.loupan.com/ask/',
+'Referer': 'http://www.baidu.com/',
+'Accept-Encoding': 'identity',
 'User-Agent':'Mozilla/5.0(Windows NT 6.1; Win64; x64)AppleWebKit/537.36(KHTML, like Gecko)Chrome/79.0.3945.79Safari/537.36',
 }
-    con_excel = 'con.xlsx'
-    CsvFile = os.path.splitext(con_excel)[0] + '采集结果.csv'
+    con_excel = 'conf.xlsx'
+    CsvFile = os.path.splitext(con_excel)[0] + '采集url-title.csv'
     IsHeader =0
     lock = threading.Lock()
     q = read_excel(con_excel)
@@ -97,8 +108,6 @@ if __name__ == "__main__":
     # 设置线程数
     for i in list(range(1)):
         t = threading.Thread(target=main)
-        t.setDaemon(True)
+        t.daemon = True
         t.start()
     q.join()
-    f.flush()
-    f.close()
